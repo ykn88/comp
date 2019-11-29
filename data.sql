70d368e7463a173b8e34d9efa74e8a84f1a8e73e
yadu <yadu@123>
2019-11-29 20:09:29 +0530

    sql

--- data.sql
+++ data.sql
@@ -4,6 +4,6 @@ create table copies (id INTEGER PRIMARY KEY, rack INTEGER, status VARCHAR(50), b
 
 create table users (id INTEGER PRIMARY KEY, name VARCHAR(50), email VARCHAR(50), phone VARCHAR(50), password VARCHAR(50), role VARCHAR(50));
 
-create table payments (id INTEGER PRIMARY KEY, amount FLOAT, type VARCHAR(50), transaction_time VARCHAR(50), nextpayment_duedate VARCHAR(50), userid INTEGER, FOREIGN KEY(userid) REFERENCES users(id));
+create table payments (id INTEGER PRIMARY KEY, amount FLOAT, type VARCHAR(50), transaction_time DATE, nextpayment_duedate DATE, userid INTEGER, FOREIGN KEY(userid) REFERENCES users(id));
 
-create table issuerecord (id INTEGER PRIMARY KEY, copyid INTEGER, FOREIGN KEY(copyid) REFERENCES copies(id), memberid INTEGER, FOREIGN KEY(memberid) REFERENCES users(id), issuedate VARCHAR(50), return_duedate VARCHAR(50), return_date VARCHAR(50), fine_amount FLOAT );
+create table issuerecord (id INTEGER PRIMARY KEY, copyid INTEGER, FOREIGN KEY(copyid) REFERENCES copies(id), memberid INTEGER, FOREIGN KEY(memberid) REFERENCES users(id), issuedate DATE, return_duedate DATE, return_date DATE, fine_amount FLOAT );
